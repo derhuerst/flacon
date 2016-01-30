77fda526bfb7a3b9aa65697bf2369ce26566c797
Jannis R <mail@jannisr.de>
2016-01-30 23:55:25 +0100

    denser code :sparkles:

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 'use strict';
 
 // A dependency injection container, holding all modules, mocks and dependencies.
-var Flacon = function () {
-
-	var modules = {},
-	notCached = {}; // `{} is …` is never true.
+function Flacon () {
+	var modules = {}, notCached = {}; // `{} is …` is never true.
 
 
 
-	var load = function (id, mocks) {
-		var module, deps;
+	var load = function (id, mocks) { // `mocks` is optional
+		var module, deps, hasMocks;
 
 		if ('string' !== typeof id) throw new Error('`id` must be a string.');
-		// `mocks` is optional
 		if (Array.isArray(mocks) || 'object' !== typeof mocks) mocks = {};
 
 		if (!modules[id]) throw new Error(id + ' has not been registered.');
 		else module = modules[id];
 
-		var hasMocks = Object.keys(mocks).length > 0;
+		hasMocks = Object.keys(mocks).length > 0;
 		if (module.cache === notCached || mocks[id]) {
+
 			// merge dependencies and mocks
 			deps = module.deps.map(function (id) {
 				var dep = load(id, mocks);
@@ -28,27 +26,26 @@ var Flacon = function () {
 				if (mocks.hasOwnProperty(id)) return mocks[id](dep);
 				else return dep;
 			});
+
 			if (hasMocks) return module.factory.apply({}, deps);
 			module.cache = module.factory.apply({}, deps);
 		}
-
 		return module.cache;
 	};
 
 
 
-	var flush = function (id) {
+	load.flush = function (id) {
 		if ('string' !== typeof id) throw new Error('`id` must be a string.');
 		if (!modules[id]) throw new Error(id + ' has not been registered.');
 
 		modules[id].cache = notCached;
-
 		return this;  // for method chaining
 	};
 
 
 
-	var publish = function (id, deps, factory) {
+	load.publish = function (id, deps, factory) {
 		if ('string' !== typeof id) throw new Error('`id` must be a string.');
 		if (arguments.length === 2) {  // `deps` is optional
 			factory = deps;
@@ -57,23 +54,14 @@ var Flacon = function () {
 		if ('function' !== typeof factory) throw new Error('`factory` must be a function.');
 
 		if (modules[id]) throw new Error(id + ' has already been registered.');
-		else modules[id] = {
-			deps:    deps,
-			factory: factory,
-			cache:   notCached
-		};
+		else modules[id] = {deps: deps, factory: factory, cache: notCached};
 
-		// To make publishing *and* exporting a factory easier, we return it here.
-		return factory;
+		return factory; // To make publishing *and* exporting a factory easier.
 	};
 
 
 
-	load.flush = flush;
-	load.publish = publish;
 	return load;
-};
-
-
+}
 
 if (module) module.exports = Flacon;
